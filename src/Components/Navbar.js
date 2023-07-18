ea00085fef252c5aa1eb77dce76ed628546913d1
TLT-1 <ncmare01@gmail.com>
2023-07-18 15:10:05 -0400

    save file

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import { Button } from './Button';
+import * as Scroll from 'react-scroll';
+
+
 
 
 function Navbar() {
@@ -13,11 +16,19 @@ function Navbar() {
 
     const showButton = () => { if (window.innerWidth <= 960) { setButton(false); } else { setButton(true); } };
 
+
+    const projectScroll = () => {
+        window.scrollTo(0, 870);
+    };
+
+
     useEffect(() => {
         showButton()
     }, []);
 
     window.addEventListener('resize', showButton);
+
+
 
     return (
         <>
@@ -27,31 +38,33 @@ function Navbar() {
                         Noah Marean {/* <i className='fab fa-typo3' />   This is the trvl logo*/}
                     </Link>
                     <div className='menu-icon' onClick={handleClick}>
+
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                     </div>
                     <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                        <li className='nav-item'>
-                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                        <li className='nav-item' onClick={closeMobileMenu}>
+                            <Link to='/' className='nav-links' onClick={Scroll.animateScroll.scrollToTop}>
                                 Home
                             </Link>
                         </li>
-                        <li className='nav-item'>
-                            <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                        <li className='nav-item' onClick={closeMobileMenu}>
+                            <Link to='/' className='nav-links' onClick={projectScroll}> {/* change this when i add the about section to the correct place */}
                                 About
                             </Link>
                         </li>
-                        <li className='nav-item'>
-                            <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
+                        <li className='nav-item' onClick={closeMobileMenu}>
+                            <Link to='/' className='nav-links' onClick={projectScroll}>
+
                                 Projects
                             </Link>
                         </li>
-                        <li className='nav-item'>
-                            <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                        <li className='nav-item' onClick={closeMobileMenu}>
+                            <Link to='/' className='nav-links' onClick={Scroll.animateScroll.scrollToBottom}>
                                 Contact
                             </Link>
                         </li>
                     </ul>
-                    {button && <Button buttonStyle='btn--primary'>Contact</Button>}
+                    {/* {button && <Button buttonStyle='btn--primary'>Contact</Button>} */}
                 </div>
             </nav>
         </>
